eaebd0920d237bdeb6ee7713f8d99697e4151741
Mindaugas Pielikis <m.pielikis@gmail.com>
2015-09-01 00:44:00 +0300

    first dirty test pass

--- Gencast.Tests/ReferenceCasterTests.cs
+++ Gencast.Tests/ReferenceCasterTests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
 
 namespace Netmf.Plus.Tests
 {
@@ -14,8 +15,8 @@ namespace Netmf.Plus.Tests
         [Test]
         public void A()
         {
-            string source =
-                @"using System; 
+            string source =@"
+                 using System; 
 
                  namespace HelloWorld 
                  { 
@@ -31,43 +32,45 @@ namespace Netmf.Plus.Tests
                          { 
                              var a = new A<string>();
 
-                             a.A = ""Hello, World!""
+                             a.A = ""Hello, World!"";
 
-                             Console.WriteLine(a.A); 
-                             Console.WriteLine(a.A); 
-                             Console.WriteLine(a.B); 
+                             Console.WriteLine(a.A);
+                             Console.WriteLine(a.B);
                          } 
                      } 
                  }";
 
-            var result =
-                @"using System; 
+            var result = 
+               @"using System; 
 
-                namespace HelloWorld 
-                { 
-                    class A
-                    {
-                        public object A { get; set; }
-                        public object A { get; set; }
-                    }
+                 namespace HelloWorld 
+                 { 
+                     class A
+                     {
+                         public object A { get; set; }
+                         public object B { get; set; }
+                     }
 
-                    class Program
-                    { 
-                        static void Main(string[] args) 
-                        { 
-                            var a = new A();
+                     class Program
+                     { 
+                         static void Main(string[] args) 
+                         { 
+                             var a = new A();
 
-                            a.A = ""Hello, World!""
+                             a.A = ""Hello, World!"";
 
-                            Console.WriteLine((string)a.A); 
-                        } 
-                    } 
-                }";
+                             Console.WriteLine((string)a.A);
+                             Console.WriteLine((string)a.B);
+                         } 
+                     } 
+                 }";
 
             var tree = CSharpSyntaxTree.ParseText(source);
-            MetadataReference mscorlib = MetadataReference.CreateFromFile("mscorlib");
-            Compilation compilation = CSharpCompilation.Create("HelloWorld")
-                .AddReferences(mscorlib)
+
+            var assemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
+
+            var compilation = CSharpCompilation.Create("HelloWorld")
+                .AddReferences(MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "mscorlib.dll")))
                 .AddSyntaxTrees(tree);
 
             // 3. Take the Semantic model of compiled tree
@@ -79,7 +82,7 @@ namespace Netmf.Plus.Tests
             var objectType = compilation.GetTypeByMetadataName("System.Object");
 
             var genericClassFinder = new GenericClassFinder(tree, semanticModel);
-            var classDeclarations = genericClassFinder.Get().ToArray();
+            var classDeclarations = genericClassFinder.Get();
 
             SyntaxNode newTree = tree.GetRoot();
             // Find reference to it's members
@@ -90,18 +93,24 @@ namespace Netmf.Plus.Tests
                 .OfType<BasePropertyDeclarationSyntax>()
                 .Select(x => (IPropertySymbol)semanticModel.GetDeclaredSymbol(x));
 
-            var memberCasts = GetCastedReferences(newTree, semanticModel, propertiesToCast).ToArray();
-            var genericChanges = ChangeGenericClasses(newTree, semanticModel, objectType).ToArray();
+            var memberCasts = GetCastedReferences(newTree, semanticModel, propertiesToCast);
+            var genericChanges = ChangeGenericClasses(newTree, semanticModel, objectType);
 
             var allChanges = memberCasts.Concat(genericChanges).ToDictionary(x => x.Key, x => x.Value);
 
-            var newTree2 = newTree.ReplaceNodes(allChanges.Keys, (x, y) => allChanges[x]);
+            var replaces = allChanges.Where(x => x.Value != null).ToDictionary(x => x.Key, x => x.Value);
+
+            var newTree2 = newTree.ReplaceNodes(replaces.Keys, (x, y) => allChanges[x]);
+
+            var newTree4 = new RemoveGenericNames().Visit(new RemoveGenericClass().Visit(newTree2));
+
+            Assert.AreEqual(result, newTree4.ToString());
 
         }
 
         private static IEnumerable<KeyValuePair<SyntaxNode, SyntaxNode>> GetCastedReferences(SyntaxNode tree, SemanticModel semanticModel, IEnumerable<IPropertySymbol> properties)
         {
-            foreach (var node in tree.ChildNodes().OfType<MemberAccessExpressionSyntax>().Where(x => x.Parent is ArgumentSyntax))
+            foreach (var node in tree.DescendantNodes().OfType<MemberAccessExpressionSyntax>().Where(x => x.Parent is ArgumentSyntax))
             {
                 ISymbol invokedSymbol = semanticModel.GetSymbolInfo(node).Symbol;
 
@@ -115,7 +124,7 @@ namespace Netmf.Plus.Tests
 
         private static IEnumerable<KeyValuePair<SyntaxNode, SyntaxNode>> ChangeGenericClasses(SyntaxNode tree, SemanticModel semanticModel, INamedTypeSymbol symbol)
         {
-            foreach (var node in tree.ChildNodes().OfType<ClassDeclarationSyntax>())
+            foreach (var node in tree.DescendantNodes().OfType<ClassDeclarationSyntax>())
             {
                 var info = semanticModel.GetDeclaredSymbol(node);
 
@@ -123,7 +132,7 @@ namespace Netmf.Plus.Tests
 
                 if (info.IsGenericType)
                 {
-                    foreach (var changedPart in GetCleanedGenericClass(tree, semanticModel, info.TypeParameters.ToArray(), symbol))
+                    foreach (var changedPart in GetCleanedGenericClass(node, semanticModel, info.TypeParameters.ToArray(), symbol))
                         yield return changedPart;
                 }
             }
@@ -131,7 +140,7 @@ namespace Netmf.Plus.Tests
 
         private static IEnumerable<KeyValuePair<SyntaxNode, SyntaxNode>> GetCleanedGenericClass(SyntaxNode tree, SemanticModel semanticModel, IEnumerable<ITypeParameterSymbol> typeParameters, INamedTypeSymbol symbol)
         {
-            foreach (var node in tree.ChildNodes().OfType<PropertyDeclarationSyntax>())
+            foreach (var node in tree.DescendantNodes().OfType<PropertyDeclarationSyntax>())
             {
                 var s = semanticModel.GetDeclaredSymbol(node);
 
@@ -143,7 +152,37 @@ namespace Netmf.Plus.Tests
                     yield return new KeyValuePair<SyntaxNode, SyntaxNode>(node.Type, typeSynax);
                 }
             }
-            
+
+            foreach (var node in tree.DescendantNodes().OfType<TypeParameterListSyntax>())
+            {
+                yield return new KeyValuePair<SyntaxNode, SyntaxNode>(node, null);
+            }
+        }
+    }
+
+    public class RemoveGenericNames : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitGenericName(GenericNameSyntax node)
+        {
+            var typeArgument = node.ChildNodes().OfType<TypeArgumentListSyntax>();
+
+            if (typeArgument.Any())
+                return SyntaxFactory.IdentifierName(node.Identifier);
+
+            return node;
+        }
+    }
+
+    public class RemoveGenericClass : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            var typeList = node.ChildNodes().OfType<TypeParameterListSyntax>();
+
+            if (typeList.Any())
+                return node.RemoveNodes(typeList, SyntaxRemoveOptions.KeepEndOfLine);
+
+            return node;
         }
     }
   
